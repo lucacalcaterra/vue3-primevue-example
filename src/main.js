618523c03a99021e148fdd6a01e239a048ba6fb5
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-05-02 20:09:32 +0200

    ok with primevue tabmenu

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import TabMenu from 'primevue/tabmenu'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/lara-light-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+//import "./index.css";
+//import "./flags.css";
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use (PrimeVue)
+app.component('TabMenu', TabMenu);
+
 
 app.mount('#app')
